37d7753b35c901f7b2ad830dac3d5b9b8b81549d
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-03 11:01:50 -0500

    Auth page completed, praise God

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import AuthPage from "./components/AuthPage";
+import ChatsPage from "./components/ChatsPage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(undefined);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  if (user) {
+    return <ChatsPage user={user} />;
+  }
+  return <AuthPage onAuth={(user) => setUser(user)} />;
 }
 
-export default App
+export default App;
