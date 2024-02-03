65f45feb87feca09215f14025e1abfb999d615c1
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-02 20:14:37 -0500

    Connected to chatengine.io, praise God

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
+require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, firstname: username },
+      { headers: { "private-key": process.env.PRIVATEKEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (err) {
+    return res.status(err.response.status).json(err.response.data);
+  }
 });
 
 app.listen(3000);
