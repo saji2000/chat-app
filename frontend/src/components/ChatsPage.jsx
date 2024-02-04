7f4ab16ff771e3539f7e7c013f377bf732bfbbbe
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-04 11:11:01 -0500

    Project finished, praise God

--- frontend/src/components/ChatsPage.jsx
+++ frontend/src/components/ChatsPage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
 
-const ChatsPage = () => {
-  return <div>ChatsPage</div>;
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "6e34749d-d102-463d-9d24-1708c15fc194",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
 };
 
 export default ChatsPage;
